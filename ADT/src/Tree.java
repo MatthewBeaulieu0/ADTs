e33143030ced530124b2e3cd79155423f09a2f61
sined631 <matthew.beaulieu631@gmail.com>
2021-10-28 13:58:50 -0400

    Start of tree implementation

---
+++ ADT/src/Tree.java
@@ -0,0 +1,15 @@
+public class Tree {
+
+    public class node {
+        private int value;
+        private node left;
+        private node right;
+
+        public node(int value){
+            this.value=value;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+}
