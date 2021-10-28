7f93c736d137c62aaf1f565b2e4ac36e1e8d365d
sined631 <matthew.beaulieu631@gmail.com>
2021-10-28 11:59:03 -0400

    PQS

--- ADT/src/PriorityQueueSelectionSort.java
+++ ADT/src/PriorityQueueSelectionSort.java
@@ -77,7 +77,10 @@ public class PriorityQueueSelectionSort {
             if (holder.next == null) throw new AbsentKeyException("Key is not part of the PQ");
             holder = holder.next;
         }
-        holder.next = holder.next.next;
+        if(holder.next!=null){
+            holder.next = holder.next.next;
+        }
+
 
 
     }
@@ -103,6 +106,23 @@ public class PriorityQueueSelectionSort {
         //Can implement method that will select a random one from all the same keys
 
     }
+    public ElementPQS removeMin()throws EmptyQueueException{
+        ElementPQS holder=this.head;
+        ElementPQS holder1=this.head,holder2=this.head;
+        if (this.head == null) throw new EmptyQueueException("Empty Queue");
+        while(holder.next!=null){
+            if(holder.next.element<holder1.element){
+                holder1=holder;// We are going to skip to next after to destroy the link
+                holder2=holder.next;
+            }
+            holder=holder.next;
+        }
+        if(holder1.next!=null){
+            holder1.next=holder1.next.next;
+        }
+
+        return holder2;
+    }
 
     public static void main(String[] args) {
         PriorityQueueSelectionSort pqs = new PriorityQueueSelectionSort();
@@ -120,7 +140,8 @@ public class PriorityQueueSelectionSort {
 
             System.out.println("Here is the value attached to key 3 : " + pqs.show(3).getElement());
             pqs.dequeue(3);
-            System.out.println("Here is the value attached to key 3 : " + pqs.show(3).getElement());
+            System.out.println("Remove minimum " + pqs.removeMin().getElement());
+            System.out.println("Remove minimum " + pqs.removeMin().getElement());
         } catch (AbsentKeyException e) {
             System.err.println(e.getMessage());
         } catch (EmptyQueueException e) {
