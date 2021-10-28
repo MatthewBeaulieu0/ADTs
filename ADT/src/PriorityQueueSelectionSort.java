f273c7a99723d50e2db41e64f109276aeb0ad028
sined631 <matthew.beaulieu631@gmail.com>
2021-10-27 20:48:53 -0400

    End of implementation of PQ

--- ADT/src/PriorityQueueSelectionSort.java
+++ ADT/src/PriorityQueueSelectionSort.java
@@ -1,10 +1,11 @@
 import QueueExceptions.*;
+import StacksException.EmptyStackException;
 
 public class PriorityQueueSelectionSort {
     /**
      * Inner Element Class
      */
-    public class ElementPQS {
+    public static class ElementPQS {
         private int key;
         private int element;
         private ElementPQS next;
@@ -65,18 +66,18 @@ public class PriorityQueueSelectionSort {
      *
      * @throws EmptyQueueException
      */
-    public void dequeue(int SKey) throws EmptyQueueException,AbsentKeyException {
+    public void dequeue(int SKey) throws EmptyQueueException, AbsentKeyException {
         if (this.head == null) throw new EmptyQueueException("Empty Queue");
-        if (this.head.key==SKey){
-            head=head.next;
+        if (this.head.key == SKey) {
+            head = head.next;
             return;
         }
-        ElementPQS holder=head;
-        while(holder.next.key!=SKey){
-            if(holder.next==null)throw new AbsentKeyException("Key is not part of the PQ");
-            holder=holder.next;
+        ElementPQS holder = head;
+        while (holder.next.key != SKey) {
+            if (holder.next == null) throw new AbsentKeyException("Key is not part of the PQ");
+            holder = holder.next;
         }
-        holder=holder.next.next;
+        holder = holder.next.next;
 
 
     }
@@ -87,22 +88,42 @@ public class PriorityQueueSelectionSort {
      * @return int front
      * @throws EmptyQueueException
      */
-    public ElementPQS show(int SKey) throws EmptyQueueException,AbsentKeyException {
+    public ElementPQS show(int SKey) throws EmptyQueueException, AbsentKeyException {
         if (this.head == null) throw new EmptyQueueException("Empty Queue");
-        if (this.head.key==SKey){
+        if (this.head.key == SKey) {
             return head;
         }
-        ElementPQS holder=head;
-        while(holder.next.key!=SKey){
-            if(holder.next==null)throw new AbsentKeyException("Key is not part of the PQ");
-            holder=holder.next;
+        ElementPQS holder = head;
+        while (holder.next.key != SKey) {
+            if (holder.next == null) throw new AbsentKeyException("Key is not part of the PQ");
+            holder = holder.next;
         }
+        holder=holder.next;
         return holder;
-
+        //Can implement method that will select a random one from all the same keys
 
     }
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        PriorityQueueSelectionSort pqs = new PriorityQueueSelectionSort();
+        ElementPQS a = new ElementPQS(1, 50, null);
+        ElementPQS b = new ElementPQS(3, 3, null);
+        ElementPQS c = new ElementPQS(4, 10, null);
+        ElementPQS d = new ElementPQS(5, 100, null);
+        pqs.enqueue(a);
+        pqs.enqueue(b);
+        pqs.enqueue(c);
+        pqs.enqueue(d);
+        System.out.println("Here is my implementation of the PQ ADT");
+        try{
+
+
+        System.out.println("Here is the value attached to key 3 : "+pqs.show(3).getElement());
+        }catch(AbsentKeyException e){
+            System.err.println(e.getMessage());
+        }catch(EmptyQueueException e){
+            System.err.println(e.getMessage());
+        }
     }
 
 
